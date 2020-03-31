1e90511d7cbca8590f9f78599f1e2949ecbd7ee9
Elizabeth Lombardi <52534564+elizabethLomb@users.noreply.github.com>
2020-03-31 20:52:44 +0200

    component UI article created, section news done

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import Article from './UI/Article';
+import Footer from '../misc/Footer';
+
 
 const News = () => {
   return(
-    <main role="main" className="mainContainer newsContainer">
-      aaaa
-    </main>
+    <div>
+      <main role="main" className="mainContainer newsContainer">
+        <section className="innerSection firstSection mt-4">
+          <div className="container">
+            <h2 className="featurette-heading display-4 mb-4 font-weight-bold">News</h2>
+
+            <Article to={'/'}
+              image={'https://www.plasticstoday.com/sites/default/files/M-Line-Muller-650.jpg'}
+              title={"Pellentesque habitant morbi tristique senectus"}
+              author={'Ait Amet,'}
+              date={'30 mar 2020'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt ex ut nunc venenatis, in tincidunt est tempus. Quisque non lacus eget lorem varius suscipit. Maecenas volutpat metus mauris, in malesuada dui lobortis ut. Vivamus ex urna, ultricies ut posuere vel, placerat eget justo.'}
+            />
+
+            <Article to={'/'}
+              image={'https://www.eppm.com/downloads/8695/download/ROBOSHOT.jpg?cb=50e80331af2d77bea7926207f7cc1c90&w=451&h='}
+              title={"Pellentesque habitant morbi tristique senectus"}
+              author={'Ait Amet,'}
+              date={'30 mar 2020'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt ex ut nunc venenatis, in tincidunt est tempus. Quisque non lacus eget lorem varius suscipit. Maecenas volutpat metus mauris, in malesuada dui lobortis ut. Vivamus ex urna, ultricies ut posuere vel, placerat eget justo.'}
+            />
+
+            <Article to={'/'}
+              image={'https://www.bccourier.com/wp-content/uploads/2020/03/Metal-Injection-Molding-Materials-1.jpg'}
+              title={"Pellentesque habitant morbi tristique senectus"}
+              author={'Ait Amet,'}
+              date={'30 mar 2020'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt ex ut nunc venenatis, in tincidunt est tempus. Quisque non lacus eget lorem varius suscipit. Maecenas volutpat metus mauris, in malesuada dui lobortis ut. Vivamus ex urna, ultricies ut posuere vel, placerat eget justo.'}
+            />
+
+            <Article to={'/'}
+              image={'https://7mz32io9r2-flywheel.netdna-ssl.com/wp-content/uploads/2019/06/adults-coffee-colleagues-1323592-1024x633.jpg'}
+              title={"Pellentesque habitant morbi tristique senectus"}
+              author={'Ait Amet,'}
+              date={'30 mar 2020'}
+              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt ex ut nunc venenatis, in tincidunt est tempus. Quisque non lacus eget lorem varius suscipit. Maecenas volutpat metus mauris, in malesuada dui lobortis ut. Vivamus ex urna, ultricies ut posuere vel, placerat eget justo.'}
+            />
+
+          </div>
+        </section>
+      </main>
+
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/components/UI/Article.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Article = ({link, image, title, author, date, text }) => {
+  return(
+    <Link to={link}>
+      <article className="article d-flex">
+        <div className="col-lg-3 col-md-3 col-12 pl-0">
+          <img src={image}/>
+        </div>
+        <div className="col-lg-9 col-md-9 col-12 pr-0">
+          <h2 className="article_title font-weight-bold">{title}</h2>
+          <small className="article_author mr-2">{author}</small>
+          <small className="article_date">{date}</small>
+          <p className="article_txt">{text}</p>
+        </div>
+      </article>
+    </Link>  
+  )
+}
+
+export default Article;
